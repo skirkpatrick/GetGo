01f8c42757c8b5bd078e9bced4b2d90a1aba53fd
Scott Kirkpatrick <skirkpatrick4@mail.csuchico.edu>
2013-05-09 19:06:13 -0700

    Initial commit. Appears to work after minimal testing

---
+++ README.md
@@ -0,0 +1,20 @@
+#GetGo
+
+A simple, poorly written, hacked-together script to install Google Go in one command!
+
+##Usage
+
+Optional:
+Customize the `GOPATH` variable for wherever you want your workspace to be. This is essentially where all of your own Go source code and packages will go.
+
+Just run the script and it'll do the rest for you. It's magical that way.
+
+If you're exceptionally lazy (like me) and don't mind the default settings, you can
+
+`curl https://raw.github.com/skirkpatrick/GetGo/master/getgo.sh | sh`
+
+It'll ask you for your `sudo` password at some point. This is because Go itself is installed into `/usr/local`. You can change this if you know what you're doing by modifying the `INSTALL` variable.
+
+##Why?
+
+Laziness. I got tired of going through the same steps everytime I installed Go on a machine.
---
+++ getgo.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env sh
+set -e
+
+if [ `uname -m` = 'x86_64' ]; then
+    ARCH='amd64'
+else
+    ARCH='386'
+fi
+
+if [ `uname` = 'Linux' ]; then
+    DISTRO=linux
+elif [ `uname` = 'Darwin' ]; then
+    DISTRO=darwin
+else
+    echo "This script does not support `uname` OS at this time"
+    exit 1
+fi
+
+INSTALL=/usr/local   # Only change this if you know what you're doing.
+GOROOT=$INSTALL/go
+GOPATH=~/gocode      # This is where your workspace will be. Change it!
+VERSION=1.0.3
+BINARY=go$VERSION.$DISTRO-$ARCH.tar.gz
+URL=https://go.googlecode.com/files/$BINARY
+
+
+echo "Downloading $BINARY from $URL"
+curl -O -L $URL
+
+echo "Installing Go into $GOROOT"
+sudo tar -C $INSTALL xzf $BINARY
+
+echo "Creating workspace at $GOPATH"
+for dir in src pkg bin
+do
+    mkdir -p $GOPATH/${dir}
+done
+
+echo "Setting environment variables"
+if [ -f ~/.bash_profile ]; then
+    PROFILE=~/.bash_profile
+else
+    PROFILE=~/.profile
+fi
+echo "export GOROOT=$GOROOT" >> $PROFILE
+echo "export GOPATH=$GOPATH" >> $PROFILE
+echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> $PROFILE
+export GOROOT
+export GOPATH
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+echo "Cleaning up"
+rm $BINARY
+
+echo "Installation complete"
